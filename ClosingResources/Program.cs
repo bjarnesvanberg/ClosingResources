6c2e8813d7ea0bfdd63059d65214bc65fcb46632
bjarnesvanberg <bjsv@easj.dk>
2014-09-01 12:12:21 +0200

    Initial commit

---
+++ ClosingResources/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosingResources
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FileName = "TestingResources.txt";
+
+            string path = Path.GetTempPath();
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string fullFileName = path + FileName;
+            if (!File.Exists(fullFileName))
+            {
+                File.Create(fullFileName);
+            }
+
+            StreamWriter toFile = new StreamWriter(fullFileName);
+            
+            Write(toFile, "Start of file");
+
+            Console.ReadKey();
+
+            toFile.Close();
+        }
+
+        public static void Write(StreamWriter sr, string text)
+        {
+            Console.WriteLine(text);
+            sr.WriteLine(text);
+        }
+    }
+}
