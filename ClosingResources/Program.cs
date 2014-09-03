3bd1fd7fe93350968992b4ffea58563d296f527f
bjarnesvanberg <bjsv@easj.dk>
2014-09-03 08:32:54 +0200

    Cleaned up the code and added comments

--- ClosingResources/Program.cs
+++ ClosingResources/Program.cs
@@ -13,24 +13,36 @@ namespace ClosingResources
         {
             const string FileName = "TestingResources.txt";
 
+            // Get the path of the "temp" folder for the current user.
             string path = Path.GetTempPath();
             if (!Directory.Exists(path))
             {
-                Directory.CreateDirectory(path);
+                // If for some reason the returned string is not a valid directory, throw an exception
+                throw new Exception(string.Format("The directory '{0}' does not exist."));
             }
+
+            // Create the full file name like this "C:\Users\username\AppData\Local\Temp\TestingResources.txt"
             string fullFileName = path + FileName;
+            StreamWriter outputFile;
+
             if (!File.Exists(fullFileName))
             {
-                File.Create(fullFileName);
+                // If the file does not exits, create it
+                outputFile = new StreamWriter( File.Create(fullFileName));
             }
-
-            StreamWriter toFile = new StreamWriter(fullFileName);
+            else
+            {
+                outputFile = new StreamWriter(fullFileName);
+            }
             
-            Write(toFile, "Start of file");
+            Write(outputFile, "Start of file");
 
+            // Wait for the user to close the program
+            Console.WriteLine("Press enter to exit.");
             Console.ReadKey();
 
-            toFile.Close();
+            // Close the allocated resource
+            outputFile.Close();
         }
 
         public static void Write(StreamWriter sr, string text)
